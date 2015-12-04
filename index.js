b6569e867b8ef75f343c2355834eeb72be4104dd
youngsterxyf <sas.198708@gmail.com>
2015-12-04 16:20:54 +0800

    添加注释

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ var urlParser = require('url');
 var childProcess = require('child_process');
 var config = require('./config.json');
 
+// 后端监控进程
 var monitorProcess = childProcess.fork('./monitor.js');
 monitorProcess.on('exit', function(code, signal) {});
 
+
+// Web服务
 function router(req, res) {
   var urlParts = urlParser.parse(req.url);
   if (urlParts.pathname === '/hello') {
--- services/storage.js
+++ services/storage.js
@@ -6,6 +6,8 @@ var sqlite3 = require('sqlite3').verbose();
 var storageFile = config.storage_file ? config.storage_file : './rsm.db';
 
 var db = new sqlite3.Database(storageFile);
+
+// 初始化,自动建表
 db.run("CREATE TABLE IF NOT EXISTS sentinels (" +
     "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
     "sentinel TEXT NOT NULL UNIQUE," +
@@ -14,6 +16,7 @@ db.run("CREATE TABLE IF NOT EXISTS sentinels (" +
 
 var storage = {
     connection: db,
+    // 更新数据库中sentinel的状态
     updateSentinelStatus: function(sentinel_addr, status) {
         this.connection.run('REPLACE INTO `sentinels` (`sentinel`, `status`) VALUES (?, ?)',
             sentinel_addr, status,
@@ -24,9 +27,11 @@ var storage = {
             }
         );
     },
+    // 从数据库中获取某个sentinel的状态
     getSentinelPreviousStatus: function(sentinel_addr, callback) {
         this.connection.get('SELECT status FROM `sentinels` WHERE sentinel=?', sentinel_addr, callback);
     },
+    // 从数据库中随便获取一个可用sentinel的地址
     getActiveSentinel: function(callback) {
         this.connection.get('SELECT sentinel FROM `sentinels` WHERE status="ON" LIMIT 1', callback);
     }
