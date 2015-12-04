6ef39d4167b47ac0c3b3808ecbb0f7f19aff968f
youngsterxyf <sas.198708@gmail.com>
2015-12-04 22:35:33 +0800

    构建时从npm目录中拷贝bootstrap的css和fonts文件

--- gulpfile.js
+++ gulpfile.js
@@ -11,10 +11,10 @@ function build() {
         .pipe(browserify())
         .pipe(gulp.dest('./assets/build'));
     // 复制文件
-
+    gulp.src([
+        './node_modules/bootstrap/dist/css/**/*.*',
+        './node_modules/bootstrap/dist/fonts/**/*.*'], {base: './node_modules/bootstrap/dist'})
+        .pipe(gulp.dest('./assets/bootstrap'));
 }
 
 gulp.task('build', build);
-gulp.task('default', function() {
-
-});
