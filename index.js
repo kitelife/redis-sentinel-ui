23255fcb53eaba5a45dc2698f6f0dfc7a202e204
gejiawen <806717031@qq.com>
2015-12-04 17:16:55 +0800

    use 4 spaces, not tab.

--- index.js
+++ index.js
@@ -3,23 +3,26 @@
 var http = require('http');
 var urlParser = require('url');
 var childProcess = require('child_process');
-var config = require('./config.json');
+
+var config = require('./config');
 
 // 后端监控进程
 var monitorProcess = childProcess.fork('./monitor.js');
-monitorProcess.on('exit', function(code, signal) {});
 
+monitorProcess.on('exit', function (code, signal) {
+    console.log('monitor progress exit with code: ' + code + ', signal: ' + signal);
+});
 
 // Web服务
 function router(req, res) {
-  var urlParts = urlParser.parse(req.url);
-  if (urlParts.pathname === '/hello') {
-    res.write('world');
+    var urlParts = urlParser.parse(req.url);
+    if (urlParts.pathname === '/hello') {
+        res.write('world');
+        res.end();
+        return;
+    }
+    res.write(urlParts.pathname);
     res.end();
-    return;
-  }
-  res.write(urlParts.pathname);
-  res.end();
 }
 
 var server = http.createServer(router);
