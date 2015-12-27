83ad2fef3a4e34c3ba0a3f60a35a591509a51c73
youngsterxyf <sas.198708@gmail.com>
2015-12-27 23:40:53 +0800

    命令输入框添加输入提示

--- controllers/cmd_page.js
+++ controllers/cmd_page.js
@@ -1,10 +1,14 @@
 'use strict';
 
+var ValidRedisCMDs = require('ioredis/commands');
+
 const Template = require('../utils/template');
 
 function _cmd_page(req, res) {
-  res.write(Template.render('views/cmd.jade'));
-  res.end();
+    res.write(Template.render('views/cmd.jade', {
+        cmdList: Object.getOwnPropertyNames(ValidRedisCMDs)
+    }));
+    res.end();
 }
 
 module.exports = _cmd_page;
