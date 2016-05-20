d66dfc8ee51b545d5312268566a9e44436321e78
youngsterxyf <sas.198708@gmail.com>
2016-05-20 18:26:41 +0800

    增强

---
+++ god.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var child_process = require('child_process');
+
+var monitor = child_process.fork('./monitor.js');
+
+var exitCallback = function (code, signal) {
+    console.log('code: ' + code + ', signal: ' + signal);
+    monitor = child_process.fork('./monitor.js');
+    monitor.on('exit', exitCallback);
+}
+
+monitor.on('exit', exitCallback);
