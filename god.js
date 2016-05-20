afb57421dbd8383e3e1df47ddf36ad464cd502b8
youngsterxyf <sas.198708@gmail.com>
2016-05-20 22:13:38 +0800

    完善

--- god.js
+++ god.js
@@ -5,9 +5,17 @@ var child_process = require('child_process');
 var monitor = child_process.fork('./monitor/monitor.js');
 
 var exitCallback = function (code, signal) {
-    console.log('code: ' + code + ', signal: ' + signal);
+    console.log('monitor.js, code: ' + code + ', signal: ' + signal);
+    if (signal === 'SIGTERM') {
+        return;
+    }
     monitor = child_process.fork('./monitor/monitor.js');
     monitor.on('exit', exitCallback);
 }
 
 monitor.on('exit', exitCallback);
+
+process.on('SIGTERM', function () {
+    console.log('god.js, SIGTERM...');
+    monitor.kill();
+});
